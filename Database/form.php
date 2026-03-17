55d2facadc1ff29bf39f1a3b00543e55b87f00dc
Emmy <cyubahiroemmy12@gmail.com>
2026-03-17 10:54:07 +0200

    Working with PHP Database Handling

---
+++ Database/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <form action="formHandle.php">
+        <label for="fname">First Name</label>
+        <input type="text" name="fname" placeholder="John">
+        <label for="lname">Last Name</label>
+        <input type="text" name="lname" placeholder="Doe">
+    </form>
+</body>
+</html>
