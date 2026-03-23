81b8fa8fbb0925af016e29ad07bf8670526cece0
Emmy <cyubahiroemmy12@gmail.com>
2026-03-23 13:08:33 +0200

    Updated my Backend

--- Database/config.php
+++ Database/config.php
@@ -1,17 +1,14 @@
 <?php
-// Database credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root'); 
-define('DB_PASSWORD', 'A@barca100%');     
-define('DB_NAME', 'auth_demo');
+define('DB_SERVER',   'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'A@barca100%');
+define('DB_NAME',     'y1b_db');
 
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-try {
-    $conn = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-} catch (PDOException $e) {
-    error_log($e->getMessage());
-    die("Database connection failed!");
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
 }
+
+echo 'Connected successfully!';
 ?>
